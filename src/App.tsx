841a759d80597064ff901ebc99671f5bd9fa6d0f
Favour Adetoun <favouradetoun@gmail.com>
2025-02-14 13:28:21 +0100

    Finished front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const date = new Date();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <header className="title zen-dots-regular">Adetoun Favour</header>
+        <div className="subHeader">
+        <p className="jobTitle">Backend Developer</p>
+        <p className="date">{date.toDateString()}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="blurb">
+          Backend developer with experience in building APIs and
+          microservices. Passionate about creating scalable and maintainable software
+          solutions.
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
