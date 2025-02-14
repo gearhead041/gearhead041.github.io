d2c7ccb422051bd71e498673f648b499d113e27a
Favour Adetoun <favouradetoun@gmail.com>
2025-02-14 19:12:25 +0100

    Added logos to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import "./App.css";
+import { GitHubLogo, GmailLogo, LinkedInLogo } from "./svg_logos";
 
 function App() {
   const date = new Date();
   return (
     <>
       <div className="header">
-        <header className="title zen-dots-regular">Adetoun Favour</header>
+        <header className="title">Adetoun Favour</header>
         <div className="subHeader">
-        <p className="jobTitle">Backend Developer</p>
-        <p className="date">{date.toDateString()}</p>
+          <p className="jobTitle">Backend Developer</p>
+          <p className="date">{date.toDateString()}</p>
+        </div>
+        <div className="logos">
+          <GitHubLogo link="https://github.com/gearhead041" />
+          <GmailLogo link="mailto:favouradetoun@gmail.com" />
+          <LinkedInLogo link="https://www.linkedin.com/in/toluadetoun/"/>
         </div>
       </div>
       <div className="blurb">
-          Backend developer with experience in building APIs and
-          microservices. Passionate about creating scalable and maintainable software
+        <p>
+          Backend developer with experience in building APIs and microservices.
+          Passionate about creating scalable and maintainable software
           solutions.
+        </p>
       </div>
     </>
   );
